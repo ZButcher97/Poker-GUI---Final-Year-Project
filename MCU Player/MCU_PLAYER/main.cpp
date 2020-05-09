96af1450b7a2162d7fe04d4f5d92cb88b2c9d12f
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-10 00:45:14 +0100

    UPDATE - Reading 1 NFC reader for card UID successful. Changes to MFRC522 library including changing initialise function to return true or false. This is done my reading a register and checking if TX1 and TX2 have been set. Added in hardware reset into MFRC object constructor to ensure hardware is reset on instance creation.

--- MCU Player/MCU_PLAYER/main.cpp
+++ MCU Player/MCU_PLAYER/main.cpp
@@ -1,23 +1,44 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include "mbed.h"
-#include "platform/mbed_thread.h"
-
-
-// Blinking rate in milliseconds
-#define BLINKING_RATE_MS                                                    500
-
+#include "main.h"
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
-
+    if(!NFC1.PCD_Init())
+    {
+        PC.printf("PCD Initalisation Failed...");
+        ThisThread::sleep_for(osWaitForever);
+    }
+    PC.printf("Initalisation Success\n\r");
+    
     while (true) {
-        led = !led;
-        thread_sleep_for(BLINKING_RATE_MS);
+        // Look for new cards
+        OnBoardLED = 1;
+
+        if (!NFC1.PICC_IsNewCardPresent())
+        {
+            wait_ms(500);
+            continue;
+        }
+    
+        // Select one of the cards
+        if (!NFC1.PICC_ReadCardSerial())
+        {
+            PC.printf("Read Card Serial Failed\n\r");
+            wait_ms(500);
+            continue;
+        }
+        OnBoardLED = 0;
+    
+        // Print Card UID
+        PC.printf("Card UID: ");
+        for (uint8_t i = 0; i < NFC1.uid.size; i++)
+        {
+            PC.printf(" %X02 ", NFC1.uid.uidByte[i]);
+        }
+        PC.printf("\n\r");
+    
+        // Print Card type
+        uint8_t piccType = NFC1.PICC_GetType(NFC1.uid.sak);
+        PC.printf("PICC Type: %s \n\r", NFC1.PICC_GetTypeName(piccType));
+        wait_ms(1000);
     }
 }
