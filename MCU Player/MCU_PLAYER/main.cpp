9fbb4302d55e8c53150ecc251a39ab5151ee5513
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-10 18:43:22 +0100

    UPDATE - Both NFC readers successfully reading card UID's individually.

--- MCU Player/MCU_PLAYER/main.cpp
+++ MCU Player/MCU_PLAYER/main.cpp
@@ -2,9 +2,8 @@
 
 int main()
 {
-    if(!NFC1.PCD_Init())
-    {
-        PC.printf("PCD Initalisation Failed...");
+    if(!init())
+    {        
         ThisThread::sleep_for(osWaitForever);
     }
     PC.printf("Initalisation Success\n\r");
@@ -12,33 +11,74 @@ int main()
     while (true) {
         // Look for new cards
         OnBoardLED = 1;
-
-        if (!NFC1.PICC_IsNewCardPresent())
-        {
-            wait_ms(500);
-            continue;
-        }
-    
-        // Select one of the cards
-        if (!NFC1.PICC_ReadCardSerial())
-        {
-            PC.printf("Read Card Serial Failed\n\r");
-            wait_ms(500);
-            continue;
-        }
-        OnBoardLED = 0;
-    
-        // Print Card UID
-        PC.printf("Card UID: ");
-        for (uint8_t i = 0; i < NFC1.uid.size; i++)
-        {
-            PC.printf(" %X02 ", NFC1.uid.uidByte[i]);
-        }
-        PC.printf("\n\r");
-    
-        // Print Card type
-        uint8_t piccType = NFC1.PICC_GetType(NFC1.uid.sak);
-        PC.printf("PICC Type: %s \n\r", NFC1.PICC_GetTypeName(piccType));
-        wait_ms(1000);
+        NFC1_Check();
+        NFC2_Check();    
+        wait_ms(10);
     }
+}
+
+
+void NFC1_Check()
+{
+    if (NFC1.PICC_IsNewCardPresent())
+    {
+        if (NFC1.PICC_ReadCardSerial())
+        {                   
+            // Print Card UID
+            PC.printf("NFC1: Card UID: ");
+            for (uint8_t i = 0; i < NFC1.uid.size; i++)
+            {
+                PC.printf(" %X", NFC1.uid.uidByte[i]);
+            }
+            PC.printf("\n\r");
+        
+            // Print Card type
+            uint8_t piccType = NFC1.PICC_GetType(NFC1.uid.sak);
+            PC.printf("NFC1: PICC Type: %s \n\r", NFC1.PICC_GetTypeName(piccType));
+        }
+        else
+        {
+            PC.printf("NFC1: Read Card Serial Failed\n\r");
+        }
+    }            
+}
+
+void NFC2_Check()
+{
+    if (NFC2.PICC_IsNewCardPresent())
+    {
+        if (NFC2.PICC_ReadCardSerial())
+        {                   
+            // Print Card UID
+            PC.printf("NFC2: Card UID: ");
+            for (uint8_t i = 0; i < NFC2.uid.size; i++)
+            {
+                PC.printf(" %X", NFC2.uid.uidByte[i]);
+            }
+            PC.printf("\n\r");
+        
+            // Print Card type
+            uint8_t piccType = NFC2.PICC_GetType(NFC2.uid.sak);
+            PC.printf("NFC2: PICC Type: %s \n\r", NFC2.PICC_GetTypeName(piccType));
+        }
+        else
+        {
+            PC.printf("NFC2: Read Card Serial Failed\n\r");
+        }
+    }            
+}
+
+bool init()
+{
+    if(!NFC1.PCD_Init())
+    {
+        PC.printf("NFC1: Initalisation Failed...\n\r");
+        return false;
+    }
+    if(!NFC2.PCD_Init())
+    {
+        PC.printf("NFC2: Initalisation Failed...\n\r");
+        return false;
+    }
+    return true; 
 }
--- MCU Player/MCU_PLAYER/main.h
+++ MCU Player/MCU_PLAYER/main.h
@@ -7,21 +7,33 @@
 #include "MFRC522.h"
 
 //Constant Defines
-#define NFC_SCLK               PB_13
-#define NFC_MISO               PB_14         //TIM16_CH1N cannot be used with SPI2 in use
-#define NFC_MOSI               PB_15         //TIM15_CH2 cannot be used with SPI2 in use
-#define NFC_RST                PB_2          //Reset for both NFC Cards
+    //SPI2
+#define NFC1_SCLK               PB_13
+#define NFC1_MISO               PB_14         //TIM16_CH1N cannot be used with SPI2 in use
+#define NFC1_MOSI               PB_15         //TIM15_CH2 cannot be used with SPI2 in use
+#define NFC1_RST                PB_2          //Reset for both NFC Cards
+#define NFC1_CS                 PB_6          //Chip select for NFC card 1
 
-#define NFC1_CS                PB_6          //Chip select for NFC card 1
-#define NFC2_CS                PB_5          //Chip select for NFC card 2
+    //SPI3
+#define NFC2_SCLK               PC_10
+#define NFC2_MISO               PC_11         //TIM16_CH1N cannot be used with SPI2 in use
+#define NFC2_MOSI               PC_12         //TIM15_CH2 cannot be used with SPI2 in use
+#define NFC2_RST                PB_7          //Reset for both NFC Cards
+#define NFC2_CS                PA_4          //Chip select for NFC card 1
+
+    //SPI
+#define SPI_SLAVE_SCLK          PB_3
 
 //Hardware Defines
-MFRC522 NFC1(NFC_MOSI, NFC_MISO, NFC_SCLK, NFC1_CS, NFC_RST);
-//MFRC522 NFC2(NFC_MOSI, NFC_MISO, NFC_SCLK, NFC2_CS, NFC_RST);
+MFRC522 NFC1(NFC1_MOSI, NFC1_MISO, NFC1_SCLK, NFC1_CS, NFC1_RST);
+MFRC522 NFC2(NFC2_MOSI, NFC2_MISO, NFC2_SCLK, NFC2_CS, NFC2_RST);
 Serial PC(USBTX, USBRX);
 DigitalOut OnBoardLED(LED_RED);
 DigitalIn Button(USER_BUTTON);
 
 //Function Prototypes
+void NFC1_Check();
+void NFC2_Check();
+bool init();
 
 #endif
