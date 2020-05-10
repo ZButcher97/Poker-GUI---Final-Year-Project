17b326b23454a04de4d4e517d19a56cad738514c
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-10 20:37:14 +0100

    UPDATE - 6 SPI busses running simultaneously. Issue with SPI1 and SPI3 collision. Use: st.com/resource/en/reference_manual/dm00031020.pdf page 873 to determin a fix for SPI1 and 3

--- MCU Board/MCU_BOARD/main.cpp
+++ MCU Board/MCU_BOARD/main.cpp
@@ -2,10 +2,35 @@
 
 int main()
 {   
-    spi.format(8,3);
-    spi.frequency(500000);
+    void SPI_INIT();
     while (true) {
-        spi.write(0x3C);
-        wait_ms(10);
+        spi1.write(0xA1);
+        spi2.write(0xB2);
+        spi3.write(0xC3);
+        spi4.write(0xD4);
+        spi5.write(0xE5);
+        spi6.write(0xF6);
+        wait_ms(100);
     }
+}
+
+void SPI_INIT()
+{
+    spi1.format(8,3);
+    spi1.frequency(500000);
+
+    spi2.format(8,3);
+    spi2.frequency(500000);
+
+    spi3.format(8,3);
+    spi3.frequency(500000);
+
+    spi4.format(8,3);
+    spi4.frequency(500000);
+
+    spi5.format(8,3);
+    spi5.frequency(500000);
+
+    spi6.format(8,3);
+    spi6.frequency(500000);
 }
--- MCU Board/MCU_BOARD/main.h
+++ MCU Board/MCU_BOARD/main.h
@@ -2,42 +2,49 @@
 #include "platform/mbed_thread.h"
 
 //SPI1
-// #define SPI_SCLK        PA_5
-// #define SPI_MISO        PA_6
-// #define SPI_MOSI        PA_7
-// #define SPI_CS          PA_4
+#define SPI1_SCLK        PA_5
+#define SPI1_MISO        PA_6
+#define SPI1_MOSI        PA_7
+#define SPI1_CS          PA_4
 
 //SPI2
-// #define SPI_SCLK        PB_13
-// #define SPI_MISO        PB_14
-// #define SPI_MOSI        PB_15
-// #define SPI_CS          PB_12
+#define SPI2_SCLK        PB_13
+#define SPI2_MISO        PB_14
+#define SPI2_MOSI        PB_15
+#define SPI2_CS          PB_12
 
-//SPI3 --To use this, Do not include CS in the SPI definition...
-#define SPI_SCLK        PB_3
-#define SPI_MISO        PB_4
-#define SPI_MOSI        PB_5
-#define SPI_CS          PB_2
+//SPI3
+#define SPI3_SCLK        PB_3
+#define SPI3_MISO        PB_4
+#define SPI3_MOSI        PB_5
+#define SPI3_CS          PB_2
 
 //SPI4
-// #define SPI_SCLK        PE_2
-// #define SPI_MISO        PE_5
-// #define SPI_MOSI        PE_6
-// #define SPI_CS          PE_4
+#define SPI4_SCLK        PE_2
+#define SPI4_MISO        PE_5
+#define SPI4_MOSI        PE_6
+#define SPI4_CS          PE_4
 
 //SPI5
-// #define SPI_SCLK        PF_7
-// #define SPI_MISO        PF_8
-// #define SPI_MOSI        PF_9
-// #define SPI_CS          PF_6
+#define SPI5_SCLK        PF_7
+#define SPI5_MISO        PF_8
+#define SPI5_MOSI        PF_9
+#define SPI5_CS          PF_6
 
 //SPI6
-// #define SPI_SCLK        PG_13
-// #define SPI_MISO        PG_12
-// #define SPI_MOSI        PG_14
-// #define SPI_CS          PG_8
+#define SPI6_SCLK        PG_13
+#define SPI6_MISO        PG_12
+#define SPI6_MOSI        PG_14
+#define SPI6_CS          PG_8
 
 // Initialise the digital pin LED1 as an output
 DigitalOut led(LED1);
 
-SPI spi(SPI_MOSI, SPI_MISO, SPI_SCLK);
+SPI spi1(SPI1_MOSI, SPI1_MISO, SPI1_SCLK);
+SPI spi2(SPI2_MOSI, SPI2_MISO, SPI2_SCLK);
+SPI spi3(SPI3_MOSI, SPI3_MISO, SPI3_SCLK);
+SPI spi4(SPI4_MOSI, SPI4_MISO, SPI4_SCLK);
+SPI spi5(SPI5_MOSI, SPI5_MISO, SPI5_SCLK);
+SPI spi6(SPI6_MOSI, SPI6_MISO, SPI6_SCLK);
+
+void SPI_INIT();
