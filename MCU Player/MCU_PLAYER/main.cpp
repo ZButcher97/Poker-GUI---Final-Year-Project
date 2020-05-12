8648d7b272d20357fd870ddb150ddb30cdff1ac8
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-13 00:04:18 +0100

    UPDATE - 2 NFC readers working with individual threads. Need to implement SPI slave into thread_MasterCom thread. SPISlave is NOT THREAD SAFE.. need to implement some for of thread synchronisation for this thread.

--- MCU Player/MCU_PLAYER/main.cpp
+++ MCU Player/MCU_PLAYER/main.cpp
@@ -1,87 +1,120 @@
 #include "main.h"
 
+//start up function
 int main()
 {
-    if(!init())
-    {        
-        ThisThread::sleep_for(osWaitForever);
-    }
-    PC.printf("Initalisation Success\n\r");
-    
+    //Set up threads
+    thread_Serial.start(callback(&eq_SerialPC, &EventQueue::dispatch_forever));
+    thread_NFC1.start(&thread_NFC1_main);    
+    thread_NFC2.start(&thread_NFC2_main);
+    thread_MasterCom.start(&thread_MasterCom_main);
+
+    //Heartbeat LED
     while (true) {
-        // Look for new cards
         OnBoardLED = 1;
-        NFC1_Check();
-        NFC2_Check();    
-        SPI_Slave.write(0x52);
-        wait_ms(10);
+        ThisThread::sleep_for(500);
+        OnBoardLED = 0;
+        ThisThread::sleep_for(500);
     }
 }
 
-
-void NFC1_Check()
+//NFC 1 thread main function
+void thread_NFC1_main()
 {
-    if (NFC1.PICC_IsNewCardPresent())
-    {
-        if (NFC1.PICC_ReadCardSerial())
-        {                   
-            // Print Card UID
-            PC.printf("NFC1: Card UID: ");
-            for (uint8_t i = 0; i < NFC1.uid.size; i++)
-            {
-                PC.printf(" %X", NFC1.uid.uidByte[i]);
-            }
-            PC.printf("\n\r");
-        
-            // Print Card type
-            uint8_t piccType = NFC1.PICC_GetType(NFC1.uid.sak);
-            PC.printf("NFC1: PICC Type: %s \n\r", NFC1.PICC_GetTypeName(piccType));
-        }
-        else
-        {
-            PC.printf("NFC1: Read Card Serial Failed\n\r");
-        }
-    }            
-}
-
-void NFC2_Check()
-{
-    if (NFC2.PICC_IsNewCardPresent())
-    {
-        if (NFC2.PICC_ReadCardSerial())
-        {                   
-            // Print Card UID
-            PC.printf("NFC2: Card UID: ");
-            for (uint8_t i = 0; i < NFC2.uid.size; i++)
-            {
-                PC.printf(" %X", NFC2.uid.uidByte[i]);
-            }
-            PC.printf("\n\r");
-        
-            // Print Card type
-            uint8_t piccType = NFC2.PICC_GetType(NFC2.uid.sak);
-            PC.printf("NFC2: PICC Type: %s \n\r", NFC2.PICC_GetTypeName(piccType));
-        }
-        else
-        {
-            PC.printf("NFC2: Read Card Serial Failed\n\r");
-        }
-    }            
-}
-
-bool init()
-{
+    //NFC 1 initalisation
     if(!NFC1.PCD_Init())
     {
-        PC.printf("NFC1: Initalisation Failed...\n\r");
-        return false;
+        eq_SerialPC.call(printf, "NFC1 Initalisation Failed... \n\r");
+        ThisThread::sleep_for(osWaitForever);        
     }
+    eq_SerialPC.call(printf, "NFC1 Initalisation Successful... \n\r");
+
+    //NFC1 Main loop
+    while(1)
+    {
+        while(!NFC1_Check()){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
+        eq_SerialPC.call(printf, "New Card 1 UID: %s\n\r", CARD1_UID.c_str());
+    }
+}
+
+//NFC 2 thread main function
+void thread_NFC2_main()
+{
     if(!NFC2.PCD_Init())
     {
-        PC.printf("NFC2: Initalisation Failed...\n\r");
-        return false;
+        eq_SerialPC.call(printf, "NFC2 Initalisation Failed... \n\r");
+        ThisThread::sleep_for(osWaitForever);        
     }
-    SPI_Slave.format(8,3);
-    SPI_Slave.frequency(500000);
-    return true; 
+    eq_SerialPC.call(printf, "NFC2 Initalisation Successful... \n\r");
+
+    while(1)
+    {
+        while(!NFC2_Check()){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
+        eq_SerialPC.call(printf, "New Card 2 UID: %s\n\r", CARD2_UID.c_str());
+    }
+}
+
+//SPI communication to Master board main loop (SPISlave)
+void thread_MasterCom_main()
+{
+    while(1)
+    {
+        ThisThread::sleep_for(osWaitForever);
+    }
+}
+
+//Check if new card is present on NFC reader 1
+bool NFC1_Check()
+{
+    if (!NFC1.PICC_IsNewCardPresent() || !NFC1.PICC_ReadCardSerial())   //Check if new card is present and readable on NFC reader
+    {
+        return false;
+    }      
+
+    char UID_received[100];
+    std::string UID_Rec;
+    for (uint8_t i = 0; i < NFC1.uid.size; i++)                         //Get new cards UID
+    {
+        sprintf(UID_received, "%X", NFC1.uid.uidByte[i]);
+        UID_Rec = UID_Rec + UID_received;
+    }
+
+    CARD1.lock();
+    if(UID_Rec == CARD1_UID)                                       //Check if current card is different from last card
+    {
+        CARD1.unlock();
+        return false;
+    }    
+    CARD1_UID = UID_Rec;                                           //Update current UID and return true;
+    CARD1.unlock();
+
+    return true;           
+}
+
+//Check if new card is present on NFC reader 2
+bool NFC2_Check()
+{
+    if (!NFC2.PICC_IsNewCardPresent() || !NFC2.PICC_ReadCardSerial())   //Check if new card is present and readable on NFC reader
+    {
+        return false;
+    }      
+
+    char UID_received[100];
+    std::string UID_Rec;
+    for (uint8_t i = 0; i < NFC2.uid.size; i++)                         //Get new cards UID
+    {
+        sprintf(UID_received, "%X", NFC2.uid.uidByte[i]);
+        UID_Rec = UID_Rec + UID_received;
+    }
+    
+    CARD2.lock();
+    if(UID_Rec == CARD2_UID)                                       //Check if current card is different from last card
+    {
+        CARD2.unlock();
+        return false;
+    }   
+    CARD2_UID = UID_Rec;                                           //Update current UID and return true;
+    CARD2.unlock();
+
+    return true;            
 }
--- MCU Player/MCU_PLAYER/main.h
+++ MCU Player/MCU_PLAYER/main.h
@@ -5,39 +5,53 @@
 #include "mbed.h"
 #include "platform/mbed_thread.h"
 #include "MFRC522.h"
+#include <string>
 
-//Constant Defines
     //SPI2
-#define NFC1_SCLK               PB_13
-#define NFC1_MISO               PB_14         //TIM16_CH1N cannot be used with SPI2 in use
-#define NFC1_MOSI               PB_15         //TIM15_CH2 cannot be used with SPI2 in use
-#define NFC1_RST                PB_2          //Reset for both NFC Cards
-#define NFC1_CS                 PB_6          //Chip select for NFC card 1
+#define     NFC1_SCLK               PB_13
+#define     NFC1_MISO               PB_14         //TIM16_CH1N cannot be used with SPI2 in use
+#define     NFC1_MOSI               PB_15         //TIM15_CH2 cannot be used with SPI2 in use
+#define     NFC1_RST                PB_2          //Reset for both NFC Cards
+#define     NFC1_CS                 PB_6          //Chip select for NFC card 1
 
     //SPI3
-#define NFC2_SCLK               PC_10
-#define NFC2_MISO               PC_11         //TIM16_CH1N cannot be used with SPI2 in use
-#define NFC2_MOSI               PC_12         //TIM15_CH2 cannot be used with SPI2 in use
-#define NFC2_RST                PB_7          //Reset for both NFC Cards
-#define NFC2_CS                 PB_5          //Chip select for NFC card 1
+#define     NFC2_SCLK               PC_10
+#define     NFC2_MISO               PC_11           //TIM16_CH1N cannot be used with SPI2 in use
+#define     NFC2_MOSI               PC_12           //TIM15_CH2 cannot be used with SPI2 in use
+#define     NFC2_RST                PB_7            //Reset for both NFC Cards
+#define     NFC2_CS                 PB_5            //Chip select for NFC card 1
 
     //SPI1
-#define SPI_SLAVE_SCLK          PA_5
-#define SPI_SLAVE_MISO          PA_6
-#define SPI_SLAVE_MOSI          PA_7
-#define SPI_SLAVE_CS            PA_4
+#define     SPI_SLAVE_SCLK          PA_5
+#define     SPI_SLAVE_MISO          PA_6
+#define     SPI_SLAVE_MOSI          PA_7
+#define     SPI_SLAVE_CS            PA_4
 
 //Hardware Defines
-MFRC522 NFC1(NFC1_MOSI, NFC1_MISO, NFC1_SCLK, NFC1_CS, NFC1_RST);
-MFRC522 NFC2(NFC2_MOSI, NFC2_MISO, NFC2_SCLK, NFC2_CS, NFC2_RST);
-SPI SPI_Slave(SPI_SLAVE_MOSI, SPI_SLAVE_MISO, SPI_SLAVE_SCLK, SPI_SLAVE_CS);
-Serial PC(USBTX, USBRX);
-DigitalOut OnBoardLED(LED_RED);
-DigitalIn Button(USER_BUTTON);
+MFRC522     NFC1(NFC1_MOSI, NFC1_MISO, NFC1_SCLK, NFC1_CS, NFC1_RST);
+MFRC522     NFC2(NFC2_MOSI, NFC2_MISO, NFC2_SCLK, NFC2_CS, NFC2_RST);
+SPI         SPI_Slave(SPI_SLAVE_MOSI, SPI_SLAVE_MISO, SPI_SLAVE_SCLK, SPI_SLAVE_CS);
+Serial      PC(USBTX, USBRX);
+DigitalOut  OnBoardLED(LED_RED);
 
 //Function Prototypes
-void NFC1_Check();
-void NFC2_Check();
-bool init();
+void        thread_NFC1_main();                     //NFC 1 thread main function
+void        thread_NFC2_main();                     //NFC 2 thread main function
+void        thread_MasterCom_main();                //SPI communication to Master board main loop (SPISlave)
+bool        NFC1_Check();                           //Check if new card is present on NFC reader 1
+bool        NFC2_Check();                           //Check if new card is present on NFC reader 2
+
+//RtOS
+Thread      thread_NFC1;
+Thread      thread_NFC2;
+Thread      thread_MasterCom;
+Thread      thread_Serial;
+EventQueue  eq_SerialPC;
+Mutex       CARD1;
+Mutex       CARD2;
+
+//Global Variables
+std::string CARD1_UID;
+std::string CARD2_UID;
 
 #endif
