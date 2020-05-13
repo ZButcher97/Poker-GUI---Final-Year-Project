feea1c266fda2a3e2abaf230310f200080b62717
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-13 07:12:08 +0100

    UPDATE - Added all NFC definitions and threads

--- MCU Board/MCU_BOARD/main.cpp
+++ MCU Board/MCU_BOARD/main.cpp
@@ -2,12 +2,14 @@
 
 int main()
 {
-    // thread_AllNFC_main();
+    printf("\n\r\n\r");
     thread_Serial.start(callback(&eq_SerialPC, &EventQueue::dispatch_forever));
-    // thread_NFC1.start(&thread_NFC1_main);
+    thread_NFC1.start(&thread_NFC1_main);
     // thread_NFC2.start(&thread_NFC2_main);
     thread_NFC3.start(&thread_NFC3_main);
     thread_NFC4.start(&thread_NFC4_main);
+    thread_NFC5.start(&thread_NFC5_main);
+    // thread_NFC6.start(&thread_NFC6_main);
 
     while (true) {
         OnBoardGreenLED = 0;
@@ -16,81 +18,53 @@ int main()
         ThisThread::sleep_for(500);
         if(button == 1)
         {
-            uint8_t val1 = NFC3.PCD_ReadRegister(MFRC522::TxControlReg);   //Initilastion check to ensure TX1 and TX2 are enabled
-            uint8_t val2 = NFC4.PCD_ReadRegister(MFRC522::TxControlReg);   //Initilastion check to ensure TX1 and TX2 are enabled
-            printf("NFC3 Antenna Register: %x\n\r", (val1 & 0x03));
-            printf("NFC4 Antenna Register: %x\n\r", (val2 & 0x03));
+            uint8_t val1 = NFC1.PCD_ReadRegister(MFRC522::TxControlReg);   //check to see if TX1 and TX2 are enabled
+            uint8_t val2 = NFC2.PCD_ReadRegister(MFRC522::TxControlReg);   
+            uint8_t val3 = NFC3.PCD_ReadRegister(MFRC522::TxControlReg);   
+            uint8_t val4 = NFC4.PCD_ReadRegister(MFRC522::TxControlReg);   
+            uint8_t val5 = NFC5.PCD_ReadRegister(MFRC522::TxControlReg);   
+            uint8_t val6 = NFC6.PCD_ReadRegister(MFRC522::TxControlReg);   
+            printf("NFC1 check: %x\t", (val1 & 0x03));
+            printf("NFC2 check: %x\t", (val2 & 0x03));
+            printf("NFC3 check: %x\t", (val3 & 0x03));
+            printf("NFC4 check: %x\t", (val4 & 0x03));
+            printf("NFC5 check: %x\t", (val5 & 0x03));
+            printf("NFC6 check: %x\n\r", (val6 & 0x03));
         }
     }
 }
-void thread_AllNFC_main()
-{
-    if(!NFC3.PCD_Init())
-    {    
-        PC.printf("NFC3 Initalisation Failed... \n\r");
-        ThisThread::sleep_for(osWaitForever);
-    }
-    PC.printf("NFC3 Initalisation Successful... \n\r");
 
-    if(!NFC4.PCD_Init())
+void thread_NFC1_main()
+{
+    if(!NFC1.PCD_Init())
     {
-         PC.printf("NFC4 Initalisation Failed... \n\r");
+        eq_SerialPC.call(printf, "NFC1 Initalisation Failed... \n\r");
         ThisThread::sleep_for(osWaitForever);
     }
-    PC.printf("NFC4 Initalisation Successful... \n\r");
+    eq_SerialPC.call(printf, "NFC1 Initalisation Successful... \n\r");
 
     while(1)
     {
-        OnBoardGreenLED = 0;
-        if(NFC_Check(NFC3)){PC.printf("New Card 3 UID: %s\n\r", NFC3.POKER_ReadCardUID().c_str());}
-        ThisThread::sleep_for(20);
-        if(NFC_Check(NFC4)){PC.printf("New Card 4 UID: %s\n\r", NFC4.POKER_ReadCardUID().c_str());}               
-        OnBoardGreenLED = 1;
-        ThisThread::sleep_for(100);
-        if(button)
-        {
-            // NFC3.PCD_Init();
-            // NFC4.PCD_Init();
-            uint8_t val1 = NFC3.PCD_ReadRegister(MFRC522::TxControlReg);   //Initilastion check to ensure TX1 and TX2 are enabled
-            uint8_t val2 = NFC4.PCD_ReadRegister(MFRC522::TxControlReg);   //Initilastion check to ensure TX1 and TX2 are enabled
-            printf("NFC3 Antenna Register: %x\n\r", (val1 & 0x03));
-            printf("NFC4 Antenna Register: %x\n\r", (val2 & 0x03));
-
-        }
+        while(!NFC_Check(NFC1)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
+        eq_SerialPC.call(printf, "New Card 1 UID: %s\n\r", NFC1.POKER_ReadCardUID().c_str());
     }
 }
 
-// void thread_NFC1_main()
-// {
-//     if(!NFC1.PCD_Init())
-//     {
-//         eq_SerialPC.call(printf, "NFC1 Initalisation Failed... \n\r");
-//         ThisThread::sleep_for(osWaitForever);
-//     }
-//     eq_SerialPC.call(printf, "NFC1 Initalisation Successful... \n\r");
+void thread_NFC2_main()
+{
+    if(!NFC2.PCD_Init())
+    {
+        eq_SerialPC.call(printf, "NFC2 Initalisation Failed... \n\r");
+        ThisThread::sleep_for(osWaitForever);
+    }
+    eq_SerialPC.call(printf, "NFC2 Initalisation Successful... \n\r");
 
-//     while(1)
-//     {
-//         while(!NFC_Check(NFC1)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
-//         eq_SerialPC.call(printf, "New Card 1 UID: %s\n\r", NFC1.POKER_ReadCardUID().c_str());
-//     }
-// }
-
-// void thread_NFC2_main()
-// {
-//     if(!NFC2.PCD_Init())
-//     {
-//         eq_SerialPC.call(printf, "NFC2 Initalisation Failed... \n\r");
-//         ThisThread::sleep_for(osWaitForever);
-//     }
-//     eq_SerialPC.call(printf, "NFC2 Initalisation Successful... \n\r");
-
-//     while(1)
-//     {
-//         while(!NFC_Check(NFC2)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
-//         eq_SerialPC.call(printf, "New Card 2 UID: %s\n\r", NFC2.POKER_ReadCardUID().c_str());
-//     }
-// }
+    while(1)
+    {
+        while(!NFC_Check(NFC2)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
+        eq_SerialPC.call(printf, "New Card 2 UID: %s\n\r", NFC2.POKER_ReadCardUID().c_str());
+    }
+}
 
 void thread_NFC3_main()
 {
@@ -103,16 +77,7 @@ void thread_NFC3_main()
 
     while(1)
     {
-        OnBoardRedLED = 0;
-        while(!NFC_Check(NFC3)){
-            if(!NFC3.IsReaderResponsive())
-            {
-                NFC3.PCD_Init();
-                eq_SerialPC.call(printf, "NFC3 Bricked! Restarting... \n\r");
-            }
-            ThisThread::sleep_for(100);
-            OnBoardRedLED = 1;
-        }               //Sleep this thread while no new card present
+        while(!NFC_Check(NFC3)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
         eq_SerialPC.call(printf, "New Card 3 UID: %s\n\r", NFC3.POKER_ReadCardUID().c_str());
     }
 }
@@ -128,11 +93,83 @@ void thread_NFC4_main()
 
     while(1)
     {
-        OnBoardBlueLED = 0;
-        while(!NFC_Check(NFC4)){        
-            ThisThread::sleep_for(100);
-             OnBoardBlueLED = 1;
-        }               //Sleep this thread while no new card present
+        while(!NFC_Check(NFC4)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
         eq_SerialPC.call(printf, "New Card 4 UID: %s\n\r", NFC4.POKER_ReadCardUID().c_str());
     }
+
 }
+
+void thread_NFC5_main()
+{
+    if(!NFC5.PCD_Init())
+    {
+        eq_SerialPC.call(printf, "NFC5 Initalisation Failed... \n\r");
+        ThisThread::sleep_for(osWaitForever);
+    }
+    eq_SerialPC.call(printf, "NFC5 Initalisation Successful... \n\r");
+
+    while(1)
+    {
+        while(!NFC_Check(NFC5)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
+        eq_SerialPC.call(printf, "New Card 5 UID: %s\n\r", NFC5.POKER_ReadCardUID().c_str());
+    }
+
+}
+
+void thread_NFC6_main()
+{
+    if(!NFC6.PCD_Init())
+    {
+        eq_SerialPC.call(printf, "NFC6 Initalisation Failed... \n\r");
+        ThisThread::sleep_for(osWaitForever);
+    }
+    eq_SerialPC.call(printf, "NFC6 Initalisation Successful... \n\r");
+
+    while(1)
+    {
+        while(!NFC_Check(NFC6)){ThisThread::sleep_for(100);}               //Sleep this thread while no new card present
+        eq_SerialPC.call(printf, "New Card 6 UID: %s\n\r", NFC6.POKER_ReadCardUID().c_str());
+    }
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                         Not used
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+
+// void thread_AllNFC_main()
+// {
+//     if(!NFC3.PCD_Init())
+//     {    
+//         PC.printf("NFC3 Initalisation Failed... \n\r");
+//         ThisThread::sleep_for(osWaitForever);
+//     }
+//     PC.printf("NFC3 Initalisation Successful... \n\r");
+
+//     if(!NFC4.PCD_Init())
+//     {
+//          PC.printf("NFC4 Initalisation Failed... \n\r");
+//         ThisThread::sleep_for(osWaitForever);
+//     }
+//     PC.printf("NFC4 Initalisation Successful... \n\r");
+
+//     while(1)
+//     {
+//         OnBoardGreenLED = 0;
+//         if(NFC_Check(NFC3)){PC.printf("New Card 3 UID: %s\n\r", NFC3.POKER_ReadCardUID().c_str());}
+//         ThisThread::sleep_for(20);
+//         if(NFC_Check(NFC4)){PC.printf("New Card 4 UID: %s\n\r", NFC4.POKER_ReadCardUID().c_str());}               
+//         OnBoardGreenLED = 1;
+//         ThisThread::sleep_for(100);
+//         if(button)
+//         {
+//             // NFC3.PCD_Init();
+//             // NFC4.PCD_Init();
+//             uint8_t val1 = NFC3.PCD_ReadRegister(MFRC522::TxControlReg);   //Initilastion check to ensure TX1 and TX2 are enabled
+//             uint8_t val2 = NFC4.PCD_ReadRegister(MFRC522::TxControlReg);   //Initilastion check to ensure TX1 and TX2 are enabled
+//             printf("NFC3 Antenna Register: %x\n\r", (val1 & 0x03));
+//             printf("NFC4 Antenna Register: %x\n\r", (val2 & 0x03));
+
+//         }
+//     }
+// }
--- MCU Board/MCU_BOARD/main.h
+++ MCU Board/MCU_BOARD/main.h
@@ -1,78 +1,86 @@
+//Includes
 #include "mbed.h"
 #include "platform/mbed_thread.h"
 #include "MFRC522.h"
 #include <string>
 
-//SPI1
+//Defines
+    //SPI1
 #define NFC1_SCLK           PA_5
 #define NFC1_MISO           PA_6
 #define NFC1_MOSI           PA_7
 #define NFC1_CS             PA_4
 #define NFC1_RST            PE_0
 
-//SPI2
+    //SPI2
 #define NFC2_SCLK           PB_13
 #define NFC2_MISO           PB_14
 #define NFC2_MOSI           PB_15
 #define NFC2_CS             PB_12
 #define NFC2_RST            PB_0
 
-//SPI3
+    //SPI3
 #define NFC3_SCLK           PB_3
 #define NFC3_MISO           PB_4
 #define NFC3_MOSI           PB_5
 #define NFC3_CS             PB_2
 #define NFC3_RST            PA_0
 
-//SPI4
+    //SPI4
 #define NFC4_SCLK           PE_2
 #define NFC4_MISO           PE_5
 #define NFC4_MOSI           PE_6
 #define NFC4_CS             PE_4
 #define NFC4_RST            PB_11
 
-//SPI5
+    //SPI5
 #define NFC5_SCLK           PF_7
 #define NFC5_MISO           PF_8
 #define NFC5_MOSI           PF_9
-#define NFC5_CS             PF_6
+#define NFC5_CS             PG_1
 #define NFC5_RST            PB_10
 
-//SPI6
+    //SPI6
 #define NFC6_MISO           PG_12
 #define NFC6_SCLK           PG_13
 #define NFC6_MOSI           PG_14
 #define NFC6_CS             PG_8
 #define NFC6_RST            PE_15
 
-// MFRC522 NFC1(NFC1_MOSI, NFC1_MISO, NFC1_SCLK, NFC1_CS, NFC1_RST);
-// MFRC522 NFC2(NFC2_MOSI, NFC2_MISO, NFC2_SCLK, NFC2_CS, NFC2_RST);
+//Hardware Definitions
+MFRC522 NFC1(NFC1_MOSI, NFC1_MISO, NFC1_SCLK, NFC1_CS, NFC1_RST);
+MFRC522 NFC2(NFC2_MOSI, NFC2_MISO, NFC2_SCLK, NFC2_CS, NFC2_RST);
 MFRC522 NFC3(NFC3_MOSI, NFC3_MISO, NFC3_SCLK, NFC3_CS, NFC3_RST);
 MFRC522 NFC4(NFC4_MOSI, NFC4_MISO, NFC4_SCLK, NFC4_CS, NFC4_RST);
+MFRC522 NFC5(NFC5_MOSI, NFC5_MISO, NFC5_SCLK, NFC5_CS, NFC5_RST);
+MFRC522 NFC6(NFC6_MOSI, NFC6_MISO, NFC6_SCLK, NFC6_CS, NFC6_RST);
 
-// Initialise the digital pin LED1 as an output
 DigitalOut OnBoardGreenLED(LED1);
 DigitalOut OnBoardBlueLED(LED2);
 DigitalOut OnBoardRedLED(LED3);
 DigitalIn button(USER_BUTTON);
+
 Serial PC(USBTX, USBRX);
 
-
-
-
-//RtOS
-// Thread      thread_NFC1;
-// Thread      thread_NFC2;
+//Threads
+Thread      thread_NFC1;
+Thread      thread_NFC2;
 Thread      thread_NFC3;
 Thread      thread_NFC4;
+Thread      thread_NFC5;
+Thread      thread_NFC6;
 Thread      thread_MasterCom;
 Thread      thread_Serial;
 Thread      thread_AllNFC;
 
+//Event Queues
 EventQueue  eq_SerialPC;
 
-// void thread_NFC1_main();
-// void thread_NFC2_main();
+//Function Prototypes
+void thread_NFC1_main();
+void thread_NFC2_main();
 void thread_NFC3_main();
 void thread_NFC4_main();
-void thread_AllNFC_main();
+void thread_NFC5_main();
+void thread_NFC6_main();
+// void thread_AllNFC_main();
