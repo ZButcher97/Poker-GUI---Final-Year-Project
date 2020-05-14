c6970efd2e4de2231ee72a2962760a192c392240
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-14 18:36:40 +0100

    UPDATE - SPI Slave com testing

--- MCU Master/MCU_MASTER/main.cpp
+++ MCU Master/MCU_MASTER/main.cpp
@@ -4,12 +4,30 @@ int main()
 {
     PlayerCOM.format(8, 0);
     PlayerCOM.frequency(8000000);
+    PlayerCS = 1;
+    ThisThread::sleep_for(5000);
+    printf("\n\r\n\rStarting Com...\n\r");
     while (true) {
+        PlayerCS = 0;
         PlayerCOM.write(0x50);
+        PlayerCS = 1;
+        wait_us(50);
+        PlayerCS = 0;
         int UID_1 = PlayerCOM.write(0x00);
+        PlayerCS = 1;
+        wait_us(50);
+        PlayerCS = 0;
         int UID_2 = PlayerCOM.write(0x00);
+        PlayerCS = 1;
+        wait_us(50);
+        PlayerCS = 0;
         int UID_3 = PlayerCOM.write(0x00);
+        PlayerCS = 1;
+        wait_us(50);
+        PlayerCS = 0;
         int UID_4 = PlayerCOM.write(0x00);
+        PlayerCS = 1;
+
         printf("Received UID: %x %x %x %x\n\r", UID_1, UID_2, UID_3, UID_4);
         ThisThread::sleep_for(1000);
     }
--- MCU Master/MCU_MASTER/main.h
+++ MCU Master/MCU_MASTER/main.h
@@ -4,11 +4,12 @@
 #include "mbed.h"
 #include "platform/mbed_thread.h"
 
-#define SPI_COM_SCLK    PA_0
-#define SPI_COM_MOSI    PA_0
-#define SPI_COM_MISO    PA_0
-#define SPI_COM_CS      PA_0
+#define SPI_COM_SCLK    PA_5
+#define SPI_COM_MOSI    PA_7
+#define SPI_COM_MISO    PA_6
+#define SPI_COM_CS      PD_14
 
 SPI PlayerCOM(SPI_COM_MOSI, SPI_COM_MISO, SPI_COM_SCLK);
+DigitalOut PlayerCS(SPI_COM_CS);
 
 #endif
