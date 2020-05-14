1e86cd2d7ec1d7eeca4f641cbfc2b91f87f78623
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-14 18:46:59 +0100

    UPDATE - SPI Slave com debugging

--- MCU Master/MCU_MASTER/main.cpp
+++ MCU Master/MCU_MASTER/main.cpp
@@ -9,26 +9,27 @@ int main()
     printf("\n\r\n\rStarting Com...\n\r");
     while (true) {
         PlayerCS = 0;
-        PlayerCOM.write(0x50);
+        int UID_1 = PlayerCOM.write(0x50);
         PlayerCS = 1;
-        wait_us(50);
-        PlayerCS = 0;
-        int UID_1 = PlayerCOM.write(0x00);
-        PlayerCS = 1;
-        wait_us(50);
-        PlayerCS = 0;
-        int UID_2 = PlayerCOM.write(0x00);
-        PlayerCS = 1;
-        wait_us(50);
-        PlayerCS = 0;
-        int UID_3 = PlayerCOM.write(0x00);
-        PlayerCS = 1;
-        wait_us(50);
-        PlayerCS = 0;
-        int UID_4 = PlayerCOM.write(0x00);
-        PlayerCS = 1;
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_1 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_2 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_3 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_4 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;
 
-        printf("Received UID: %x %x %x %x\n\r", UID_1, UID_2, UID_3, UID_4);
+        // printf("Received UID: %x %x %x %x\n\r", UID_1, UID_2, UID_3, UID_4);
+        printf("Received UID: %x\n\r", UID_1);
         ThisThread::sleep_for(1000);
     }
 }
