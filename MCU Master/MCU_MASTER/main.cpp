cb58885088a1759abb773b5691663a53d9f32b1c
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-18 15:24:53 +0100

    SPI com testing

--- MCU Master/MCU_MASTER/main.cpp
+++ MCU Master/MCU_MASTER/main.cpp
@@ -2,50 +2,32 @@
 
 int main()
 {
-    const int addr7bit = 0x48;      // 7 bit I2C address
-    const int addr8bit = 0x48 << 1; // 8bit I2C address, 0x90
    
-    char cmd[2];
-    while (1) {
-        cmd[0] = 0x01;
-        cmd[1] = 0x00;
-        TestI2C.write(addr8bit, cmd, 2);
- 
-        wait(0.5);
- 
-        cmd[0] = 0x00;
-        TestI2C.write(addr8bit, cmd, 1);
-        TestI2C.read( addr8bit, cmd, 2);
-        
-        printf("Returned Data: [1]->0x%x\t[2]->0x%x", cmd[0], cmd[1]);
+    PlayerCOM.format(8, 0);
+    PlayerCOM.frequency(100000);
+    ThisThread::sleep_for(5000);
+    printf("\n\r\n\rStarting Com...\n\r");
+    while (true) {
+        int UID_1 = PlayerCOM.write(0xA6);
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_1 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_2 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;0
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_3 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;
+        // wait_us(50);
+        // PlayerCS = 0;
+        // int UID_4 = PlayerCOM.write(0x00);
+        // PlayerCS = 1;
 
+        // printf("Received UID: %x %x %x %x\n\r", UID_1, UID_2, UID_3, UID_4);
+        printf("Received UID: %x\n\r", UID_1);
+        ThisThread::sleep_for(1000);
     }
-   
-    // PlayerCOM.format(8, 0);
-    // PlayerCOM.frequency(100000);
-    // ThisThread::sleep_for(5000);
-    // printf("\n\r\n\rStarting Com...\n\r");
-    // while (true) {
-    //     int UID_1 = PlayerCOM.write(0x5B);
-    //     // wait_us(50);
-    //     // PlayerCS = 0;
-    //     // int UID_1 = PlayerCOM.write(0x00);
-    //     // PlayerCS = 1;
-    //     // wait_us(50);
-    //     // PlayerCS = 0;
-    //     // int UID_2 = PlayerCOM.write(0x00);
-    //     // PlayerCS = 1;
-    //     // wait_us(50);
-    //     // PlayerCS = 0;
-    //     // int UID_3 = PlayerCOM.write(0x00);
-    //     // PlayerCS = 1;
-    //     // wait_us(50);
-    //     // PlayerCS = 0;
-    //     // int UID_4 = PlayerCOM.write(0x00);
-    //     // PlayerCS = 1;
-
-    //     // printf("Received UID: %x %x %x %x\n\r", UID_1, UID_2, UID_3, UID_4);
-    //     printf("Received UID: %x\n\r", UID_1);
-    //     ThisThread::sleep_for(1000);
-    // }
 }
--- MCU Master/MCU_MASTER/main.h
+++ MCU Master/MCU_MASTER/main.h
@@ -4,14 +4,12 @@
 #include "mbed.h"
 #include "platform/mbed_thread.h"
 
-#define SPI_COM_SCLK    PA_5
-#define SPI_COM_MOSI    PA_7
-#define SPI_COM_MISO    PA_6
-#define SPI_COM_CS      PA_4
+#define SPI_COM_SCLK    PE_2
+#define SPI_COM_MOSI    PE_6
+#define SPI_COM_MISO    PE_5
+#define SPI_COM_CS      PE_4
 
-#define I2C_SCL         PF_1
-#define I2C_SDA         PF_0
 
-// SPI PlayerCOM(SPI_COM_MOSI, SPI_COM_MISO, SPI_COM_SCLK, SPI_COM_CS);
-I2C TestI2C(I2C_SDA, I2C_SCL);
+SPI PlayerCOM(SPI_COM_MOSI, SPI_COM_MISO, SPI_COM_SCLK, SPI_COM_CS);
+// I2C TestI2C(I2C_SDA, I2C_SCL);
 #endif
