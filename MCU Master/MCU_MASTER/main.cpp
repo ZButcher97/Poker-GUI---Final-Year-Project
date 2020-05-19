54f036335da46a1fa352d9241736c6fe1d70d818
Zack Butcher <butcher97@hotmail.co.uk>
2020-05-19 11:36:44 +0100

    Spi testing

--- MCU Master/MCU_MASTER/main.cpp
+++ MCU Master/MCU_MASTER/main.cpp
@@ -5,10 +5,16 @@ int main()
    
     PlayerCOM.format(8, 0);
     PlayerCOM.frequency(100000);
+    CS = 1;
     ThisThread::sleep_for(5000);
     printf("\n\r\n\rStarting Com...\n\r");
     while (true) {
-        int UID_1 = PlayerCOM.write(0xA6);
+        CS = 0;
+        wait_us(10);
+        int UID_1 = PlayerCOM.write(0xFE);
+        wait_us(10);
+        CS = 1;
+        wait_us(10);
         // wait_us(50);
         // PlayerCS = 0;
         // int UID_1 = PlayerCOM.write(0x00);
